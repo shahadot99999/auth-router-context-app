eb39a9318981fe6b88ee96402133eaeea387cd97
shahadot99999 <shahadot99999@gmail.com>
2022-10-17 02:40:37 +0600

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../contexts/UserContext';
+
+const Header = () => {
+    const { user, logOut } = useContext(AuthContext);
+    console.log('context', user);
+
+    const handleSignOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(error => console.error(error));
+    }
+
+    return (
+        <div>
+            <div className="navbar bg-neutral text-neutral-content">
+                <Link className="btn btn-ghost normal-case text-xl">KLA Auth</Link>
+                <Link className="btn btn-ghost normal-case text-xl" to='/'>Home</Link>
+
+                <Link className="btn btn-ghost normal-case text-xl" to='/orders'>Orders</Link>
+                <Link className="btn btn-ghost normal-case text-xl" to={'/login'}>Log In</Link>
+                <Link className="btn btn-ghost normal-case text-xl" to={'/register'}>Register</Link>
+                {user?.email && <span>Welcome{user.email}</span>}
+                {
+                    user?.email ?
+                        <button onClick={handleSignOut} className="btn btn-sm">Sign Out</button>
+                        : <Link to='/login'>
+                            <button className="btn btn-sm">Log In</button>
+                        </Link>
+                }
+
+            </div>
+
+        </div>
+    );
+};
+
+export default Header; 
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyATw9ENhhP3Zby4FPFPkdAeJT2p_RPpanw",
+    authDomain: "auth-router-context-60a7d.firebaseapp.com",
+    projectId: "auth-router-context-60a7d",
+    storageBucket: "auth-router-context-60a7d.appspot.com",
+    messagingSenderId: "624837614367",
+    appId: "1:624837614367:web:939b44f1abda5758452e2a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
